010aefcad14d95ced2a01ca966ae8e8a9ff081da
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-21 18:28:31 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,33 +6,33 @@ require_relative './course.rb'
 class Scraper
    #get_page method will be responsible for using Nokogiri and open-uri
    #to grab the entire HTML document from the web page
-   def get_page
+         def get_page
+           doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+         end 
 
-  doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+        def get_courses
+          self.get_page.css(".post") # return the collection of Nokogiri XML elements that describe each course
+        end
 
-
-
-
-  def get_courses
-    self.get_page.css(".post")
-  end
-#  doc.css(".post").each do |post|
-#    course = Course.new
-#    course.title = post.css("h2").text #i found these elements by inspecting the page and using pry 
-#    course.schedule = post.css(".date").text
-#    course.description = post.css("p").text
-  end
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
+        def make_courses
+          doc.css(".post").each do |post|
+          course = Course.new
+          course.title = post.css("h2").text #i found these elements by inspecting the page and using pry
+          course.schedule = post.css(".date").text
+          course.description = post.css("p").text
+        end 
+        
+        def print_courses
+        self.make_courses
+        Course.all.each do |course|
+          if course.title
+            puts "Title: #{course.title}"
+            puts "  Schedule: #{course.schedule}"
+            puts "  Description: #{course.description}"
+          end
+        end
       end
-    end
-  end
 
-end
+end # end scraper class 
 
 Scraper.new.get_page
