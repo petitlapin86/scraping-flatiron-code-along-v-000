9ee722ddcb5ff6db094b8bba481964a139def8c0
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-21 18:10:39 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,9 @@ class Scraper
    #get_page method will be responsible for using Nokogiri and open-uri
    #to grab the entire HTML document from the web page
    def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))  
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+ binding.pry
   end
     def print_courses
     self.make_courses
